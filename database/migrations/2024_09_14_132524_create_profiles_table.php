a5bc45b6ee32639229483773eabd0125ce665dcb
Julien Willy <julien.willy@gmail.com>
2024-09-14 15:44:04 +0200

    [UPDATE] - Update migrations for Administrators and Profiles tables

--- database/migrations/2024_09_14_132524_create_profiles_table.php
+++ database/migrations/2024_09_14_132524_create_profiles_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('firstname');
+            $table->string('image');
+            $table->enum('status', ['inactive', 'pending', 'active']);
             $table->timestamps();
         });
     }
