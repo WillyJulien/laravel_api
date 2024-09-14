ff0d6ea098092bb131923e2b0a4b410b1cb8ff4b
Julien Willy <julien.willy@gmail.com>
2024-09-14 15:45:12 +0200

    [UPDATE] - AdministratorController

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -5,45 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Administrator;
 use Illuminate\Http\Request;
 
-class AdministratorController extends Controller
-{
+class AdministratorController extends Controller {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+    * Display a listing of the resource.
+    */
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function login( Request $request ) {
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Administrator $administrator)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Administrator $administrator)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Administrator $administrator)
-    {
-        //
     }
 }
