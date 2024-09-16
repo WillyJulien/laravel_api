510b51ac98962f144d903f89d8caab704b2298ce
Julien Willy <julien.willy@gmail.com>
2024-09-16 20:03:00 +0200

    [WIP] - Clean file

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 use App\Http\Requests\ProfileRequest;
 use App\Enums\ProfileStatus;
 use Laravel\Sanctum\PersonalAccessToken;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller {
     /**
